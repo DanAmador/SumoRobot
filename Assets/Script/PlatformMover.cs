cf27ebe2c30f19be7b0eb7b9a395d38d30578a3e
Dan Amador <danamadorpe@gmail.com>
2019-05-28 09:50:17 +0200

    Initial commit

---
+++ Assets/Script/PlatformMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour
+{
+    private Transform platform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var transformRotation = this.transform.rotation;
+    }
+}

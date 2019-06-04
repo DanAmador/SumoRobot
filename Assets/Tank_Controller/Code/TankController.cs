d01d501fbae803d83b8f8814d8919f09899f7ac7
Dan Amador <danamadorpe@gmail.com>
2019-06-04 22:33:21 +0200

    Made some sweet hover cars

--- Assets/Tank_Controller/Code/TankController.cs
+++ Assets/Tank_Controller/Code/TankController.cs
@@ -10,9 +10,10 @@ namespace Tank_Controller {
 
         public TankState state;
         [Range(1, 10f)] private float maxSpeed = 8f;
-        [Header("Movement Properties")] public float tankRotationSpeed = 20f;
+        [Header("Movement Properties")] public float turnSpeed = 5f;
         [SerializeField] private float currSpeed, specialCounter;
-
+        public float hoverForce = 65f;
+        public float hoverHeight = 2f;
         private Rigidbody rb;
         private TankInputs input;
 
@@ -29,9 +30,9 @@ namespace Tank_Controller {
         private void Update() {
             specialCounter = Mathf.Clamp(specialCounter + Time.deltaTime, 0, 5);
             if (state == TankState.NORMAL) {
-                currSpeed += input.ForwardInput * 3 * Time.deltaTime -
-                             (1 - Mathf.Abs(input.ForwardInput)) * Time.deltaTime * 10;
-                currSpeed = Mathf.Clamp(currSpeed, 3, maxSpeed);
+//                currSpeed += input.ForwardInput * 3 * Time.deltaTime -
+//                             (1 - Mathf.Abs(input.ForwardInput)) * Time.deltaTime * 10;
+//                currSpeed = Mathf.Clamp(currSpeed, 3, maxSpeed);
 
                 if (input.Turbo) {
                     StartCoroutine(TurboBoost(specialCounter));
@@ -78,17 +79,30 @@ namespace Tank_Controller {
         }
 
         protected virtual void HandleMovement() {
-            rb.MovePosition(transform.position + (currSpeed * Time.deltaTime * input.ForwardInput * transform.forward));
-            Quaternion wantedRotation = transform.rotation *
-                                        Quaternion.Euler(input.RotationInput * Time.deltaTime * tankRotationSpeed *
-                                                         Vector3.up);
-            rb.MoveRotation(wantedRotation);
+//            rb.MovePosition(transform.position + (currSpeed * Time.deltaTime * input.ForwardInput * transform.forward));
+//            Quaternion wantedRotation = transform.rotation *
+//                                        Quaternion.Euler(input.RotationInput * Time.deltaTime * turnSpeed *
+//                                                         Vector3.up);
+//            rb.MoveRotation(wantedRotation);
+//            
+            
+            Ray ray = new Ray(transform.position, -transform.up);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, hoverHeight)) {
+                float proportionalHeight = (hoverHeight - hit.distance) / hoverHeight;
+                Vector3 appliedHoverForce = Vector3.up * proportionalHeight * hoverForce;
+                rb.AddForce(appliedHoverForce, ForceMode.Acceleration);
+            }
+
+            rb.AddRelativeForce(0f, 0f, input.ForwardInput * currSpeed);
+            rb.AddRelativeTorque(0f, input.RotationInput * turnSpeed, 0f);
         }
 
 
         void OnCollisionEnter(Collision collision) {
             if (collision.gameObject.tag == "Player") {
-                Debug.Log("Ye");
+                TankController collider = collision.gameObject.GetComponent<TankController>();
             }
         }
 

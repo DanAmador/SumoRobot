ca19ef716c11e6e724837000b457bfcd55edb1e5
Dan Amador <danamadorpe@gmail.com>
2019-06-07 10:15:46 +0200

    Tank asset updated

--- Assets/Tank_Controller/Code/TankController.cs
+++ Assets/Tank_Controller/Code/TankController.cs
@@ -11,48 +11,45 @@ namespace Tank_Controller {
     public class TankController : MonoBehaviour {
         #region Variables
 
-        public TankState state;
         [Header("Movement Properties")] public float turnSpeed = 5f;
+        public float rotationRate, turnRotationAngle, turnRotationSeekSpeed;
 
-        [SerializeField] private float currSpeed, specialCounter;
+        public TankState state;
 
-        [Range(1, 10f)] private float maxSpeed = 8f;
         [Range(3, 10)] private const float MAX_SPECIAL = 5;
         [Range(700, 2000)] private const float MAX_SPEED = 1500;
         [Range(150, 500)] private const float START_SPEED = 200;
+        [SerializeField] private float currSpeed, specialCounter;
+        private float _rotationVelocity, _groundAngleVelocity;
 
-        private Rigidbody rb;
-        private TankInputs input;
-        private Thruster thrusters;
+        private Rigidbody _rb;
+        private TankInputs _input;
+        private Thruster _thrusters;
 
 
-        public float rotationRate;
-        public float turnRotationAngle, turnRotationSeekSpeed;
-
-        private float rotationVelocity, groundAngleVelocity;
 
         #endregion
 
         #region Mono Methods
 
         void Start() {
-            rb = GetComponent<Rigidbody>();
-            input = GetComponent<TankInputs>();
-            thrusters = GetComponent<Thruster>();
+            _rb = GetComponent<Rigidbody>();
+            _input = GetComponent<TankInputs>();
+            _thrusters = GetComponent<Thruster>();
             state = TankState.NORMAL;
         }
 
         private void Update() {
             specialCounter = Mathf.Clamp(specialCounter + Time.deltaTime, 0, MAX_SPECIAL);
             if (state == TankState.NORMAL) {
-                currSpeed = Mathf.Clamp(currSpeed + Mathf.Abs(input.ForwardInput) * currSpeed * Time.deltaTime, 1,
+                currSpeed = Mathf.Clamp(currSpeed + Mathf.Abs(_input.ForwardInput) * currSpeed * Time.deltaTime, 1,
                     MAX_SPEED);
 
-                if (input.Turbo) {
+                if (_input.Turbo) {
                     StartCoroutine(TurboBoost(specialCounter));
                 }
 
-                if (input.Block) {
+                if (_input.Block) {
                     StartCoroutine(BlockRoutine());
                 }
             }
@@ -64,7 +61,7 @@ namespace Tank_Controller {
         }
 
         void FixedUpdate() {
-            if (rb && input) {
+            if (_rb && _input) {
                 HandleMovement();
             }
         }
@@ -75,26 +72,26 @@ namespace Tank_Controller {
         #region Custom Code
 
         protected virtual void HandleMovement() {
-            if (Physics.Raycast(transform.position, transform.up * -1, thrusters.distance + 3)) {
-                rb.drag = 1;
+            if (Physics.Raycast(transform.position, transform.up * -1, _thrusters.distance + 3)) {
+                _rb.drag = 1;
 
-                Vector3 forwardForce = currSpeed * input.ForwardInput * transform.forward;
-                forwardForce = Time.deltaTime * rb.mass * forwardForce;
-                rb.AddForce(forwardForce);
+                Vector3 forwardForce = currSpeed * _input.ForwardInput * transform.forward;
+                forwardForce = Time.deltaTime * _rb.mass * forwardForce;
+                _rb.AddForce(forwardForce);
             }
             else {
-                rb.drag = 0;
+                _rb.drag = 0;
             }
 
 
-            Vector3 turnTorque = rotationRate * input.RotationInput * Vector3.up;
+            Vector3 turnTorque = rotationRate * _input.RotationInput * Vector3.up;
 
-            turnTorque = Time.deltaTime * rb.mass * turnTorque;
-            rb.AddTorque(turnTorque);
+            turnTorque = Time.deltaTime * _rb.mass * turnTorque;
+            _rb.AddTorque(turnTorque);
 
             Vector3 newRotation = transform.eulerAngles;
-            newRotation.z = Mathf.SmoothDampAngle(newRotation.z, input.ForwardInput * -turnRotationAngle,
-                ref rotationVelocity, turnRotationSeekSpeed);
+            newRotation.z = Mathf.SmoothDampAngle(newRotation.z, _input.ForwardInput * -turnRotationAngle,
+                ref _rotationVelocity, turnRotationSeekSpeed);
             transform.eulerAngles = newRotation;
         }
 
@@ -110,21 +107,21 @@ namespace Tank_Controller {
 
         private IEnumerator BlockRoutine() {
             state = TankState.BLOCK;
-            rb.constraints = RigidbodyConstraints.FreezeAll;
+            _rb.constraints = RigidbodyConstraints.FreezeAll;
             yield return new WaitUntil(BlockPredicate);
 
             state = TankState.NORMAL;
-            rb.constraints = RigidbodyConstraints.None;
+            _rb.constraints = RigidbodyConstraints.None;
         }
 
         private bool BlockPredicate() {
-            return !input.Block || specialCounter <= 0;
+            return !_input.Block || specialCounter <= 0;
         }
 
         private IEnumerator TurboBoost(float currentSpecial) {
             float fraction = currentSpecial / MAX_SPECIAL;
             state = TankState.BOOST;
-            currSpeed = currSpeed + (maxSpeed * fraction);
+            currSpeed = currSpeed + (MAX_SPEED * fraction);
             yield return new WaitForSeconds(0.2f);
 
             state = TankState.NORMAL;

6ad52c11952fd4e00fd21ecd34cf7c1f26433043
Dan Amador <danamadorpe@gmail.com>
2019-06-10 17:44:12 +0200

    Different academy used for tackling

---
+++ Assets/Entities/Tank/Code/AI/TankTackleAcademy.cs
@@ -0,0 +1,15 @@
+﻿using MLAgents;
+
+namespace Tank.AI {
+    public class TankTackleAcademy : Academy {
+        public float EnemySpawnVariance;
+
+        public override void AcademyReset() {
+            EnemySpawnVariance = resetParameters["EnemySpawnVariance"];
+        }
+
+        public override void InitializeAcademy() {
+            Monitor.SetActive(true);
+        }
+    }
+}

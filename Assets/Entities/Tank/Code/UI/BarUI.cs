7a4019158234679d0abb9edc86fe5c6ac2f7f551
Dan Amador <danamadorpe@gmail.com>
2019-08-18 16:29:44 +0200

    Added UI Bars

---
+++ Assets/Entities/Tank/Code/UI/BarUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Tank.UI {
+    public class BarUI : MonoBehaviour {
+        private TankController _tank;
+        public Image speed, special;
+
+        void Start() {
+            _tank = GetComponentInParent<TankController>();
+
+        }
+
+        void Update() {
+            special.fillAmount = _tank.GetNormalizedSpecial();
+            speed.fillAmount = _tank.GetNormalizedSpeed();
+        }
+    }
+}

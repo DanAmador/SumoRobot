2a76ee8044bad35066b9acb888ad7ca54a38f239
Dan Amador <danamadorpe@gmail.com>
2019-08-19 10:18:58 +0200

    Started working on the star system

---
+++ Assets/Entities/Platform/Code/Star.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Platform {
+    public class Star : MonoBehaviour {
+        private Vector3 _direction;
+        private Starfield _starfield;
+        public int? ConstellationNum { get; private set; }
+        private float _speed;
+
+        void Start() {
+            _speed = Random.Range(0.01f, .05f);
+            _direction = Random.onUnitSphere;
+        }
+
+        void Update() {
+            transform.RotateAround(transform.parent.position, _direction, _speed);
+//            transform.Rotate(_direction, _speed * Time.deltaTime);
+        }
+
+
+        private void OnTriggerEnter(Collider other) {
+            if (_starfield != null) {
+                Star stother = other.gameObject.GetComponent<Star>();
+                if (stother.ConstellationNum != null) {
+                    ConstellationNum = stother.ConstellationNum;
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other) {
+            ConstellationNum = null;
+        }
+
+        public void AddStarfield(Starfield starfield) {
+            _starfield = starfield;
+        }
+    }
+}
---
+++ Assets/Entities/Platform/Code/Starfield.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platform {
+    public class Starfield : MonoBehaviour {
+        public int amountOfStars = 50;
+        public GameObject starObject;
+        private List<Star> _stars;
+
+        public float radius = 25;
+
+        void Start() {
+            _stars = new List<Star>();
+
+            for (int i = 0; i < amountOfStars; i++) {
+                Vector3 spawnPos = Random.onUnitSphere * radius;
+                GameObject st = Instantiate(starObject, spawnPos, Quaternion.identity);
+                st.transform.parent = transform;
+                Star star = st.GetComponent<Star>();
+                star.AddStarfield(this);
+                _stars.Add(star);
+            }
+        }
+
+        // Update is called once per frame
+        void Update() { }
+    }
+}

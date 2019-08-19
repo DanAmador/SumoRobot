612a4cb5cab3457319083036abf84e431d23568a
Dan Amador <danamadorpe@gmail.com>
2019-08-19 16:39:20 +0200

    Star Logic implemented

--- Assets/Entities/Platform/Code/Star.cs
+++ Assets/Entities/Platform/Code/Star.cs
@@ -1,40 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 namespace Platform {
     public class Star : MonoBehaviour {
-        private Vector3 _direction;
-        private Starfield _starfield;
-        public int? ConstellationNum { get; private set; }
+        private Vector3 _direction,_lookDirection;
         private float _speed;
-
-        void Start() {
+        [SerializeField] private HashSet<Star> directNeighbors;
+        void Awake() {
+            directNeighbors = new HashSet<Star>();
             _speed = Random.Range(0.01f, .05f);
             _direction = Random.onUnitSphere;
+            _lookDirection = Random.onUnitSphere * Random.Range(0,30);
         }
 
         void Update() {
+            if (directNeighbors == null)
+                directNeighbors = new HashSet<Star>(); //I have no fucking clue why it wasn't being initialized...
+            if (transform.position.y < -2) {
+                _direction *= -1;
+            }
+
             transform.RotateAround(transform.parent.position, _direction, _speed);
+            transform.LookAt(transform.parent.position + Random.onUnitSphere * Mathf.Sin(Time.deltaTime));
 //            transform.Rotate(_direction, _speed * Time.deltaTime);
+
+            foreach (Star neighbor in directNeighbors) {
+                Debug.DrawLine(neighbor.transform.position, transform.position, Color.yellow);
+            }
         }
 
 
         private void OnTriggerEnter(Collider other) {
-            if (_starfield != null) {
+            if (directNeighbors != null && other.gameObject.CompareTag("Star")) {
                 Star stother = other.gameObject.GetComponent<Star>();
-                if (stother.ConstellationNum != null) {
-                    ConstellationNum = stother.ConstellationNum;
-                }
+                if (!stother.HasDirectNeighor(this)) directNeighbors.Add(stother);
             }
         }
 
         private void OnTriggerExit(Collider other) {
-            ConstellationNum = null;
+            directNeighbors.Remove(other.gameObject.GetComponent<Star>());
         }
 
-        public void AddStarfield(Starfield starfield) {
-            _starfield = starfield;
+        public bool HasDirectNeighor(Star star) {
+            return directNeighbors.Contains(star);
         }
     }
 }
--- Assets/Entities/Platform/Code/Starfield.cs
+++ Assets/Entities/Platform/Code/Starfield.cs
@@ -6,23 +6,20 @@ namespace Platform {
         public int amountOfStars = 50;
         public GameObject starObject;
         private List<Star> _stars;
-
         public float radius = 25;
+        private int _counter;
 
         void Start() {
             _stars = new List<Star>();
 
             for (int i = 0; i < amountOfStars; i++) {
                 Vector3 spawnPos = Random.onUnitSphere * radius;
+                spawnPos.y = Mathf.Abs(spawnPos.y);
                 GameObject st = Instantiate(starObject, spawnPos, Quaternion.identity);
                 st.transform.parent = transform;
                 Star star = st.GetComponent<Star>();
-                star.AddStarfield(this);
                 _stars.Add(star);
             }
         }
-
-        // Update is called once per frame
-        void Update() { }
     }
 }

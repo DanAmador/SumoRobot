c0326f570772783089768393a9ae64578fae6d9b
Dan Amador <danamadorpe@gmail.com>
2019-09-03 17:24:59 +0200

    Added countdown

--- Assets/Entities/GameSession/GameSessionManager.cs
+++ Assets/Entities/GameSession/GameSessionManager.cs
@@ -2,25 +2,37 @@
 using System.Linq;
 using Platform;
 using Tank;
+using TMPro;
 using UnityEngine;
 
 namespace GameSession {
     public class GameSessionManager : MonoBehaviour {
         [SerializeField] private TankController[] tanks;
+        public TextMeshProUGUI txt;
         private PlatformMover _platform;
         private float _matchStart;
         public float roundDuration = 120; // in seconds 
 
         public float MatchPercentageRemaining => 1 - (Time.time - _matchStart) / roundDuration;
-        public float SecondsRemaining => roundDuration - (Time.time - _matchStart);
 
-        
+        private String SecondsRemaining =>
+            (Mathf.Clamp(roundDuration - (Time.time - _matchStart), 0, roundDuration)).ToString("0.00");
 
-        public void Awake() {    
+        private bool _colorUpdated = false;
+
+
+        public void Awake() {
             _platform = GetComponentInChildren<PlatformMover>();
             _matchStart = Time.time;
         }
 
+        public void Update() {
+            txt.text = SecondsRemaining;
+            if (MatchPercentageRemaining < .3f && !_colorUpdated) {
+                txt.color = new Color(1f, 0.49f, 0.73f);
+                _colorUpdated = true;
+            }
+        }
 
         public TankController GetEnemy(TankController player) {
             return tanks.FirstOrDefault(tank => !tank.Equals(player));
@@ -30,8 +42,11 @@ namespace GameSession {
             foreach (TankController tank in tanks) {
                 tank.Reset();
             }
+
             _matchStart = Time.time;
             _platform.Reset();
+            _colorUpdated = false;
+            txt.color = Color.white;
         }
     }
 }

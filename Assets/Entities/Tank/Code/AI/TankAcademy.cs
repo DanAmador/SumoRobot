8e8e5e2d4cfd1e930cf4a2a73140931bb2cc34d9
Dan Amador <danamadorpe@gmail.com>
2020-01-02 21:36:34 +0100

    bruh?

--- Assets/Entities/Tank/Code/AI/TankAcademy.cs
+++ Assets/Entities/Tank/Code/AI/TankAcademy.cs
@@ -1,10 +1,18 @@
 ﻿using MLAgents;
+using UnityEngine;
 
 namespace Tank.AI {
     public class TankAcademy : Academy {
         
         public override void InitializeAcademy() {
 //            Monitor.SetActive(true);
+            
+            // We increase the Physics solver iterations in order to
+            // make thruster calculations more accurate.
+            Physics.defaultSolverIterations = 12;
+            Physics.defaultSolverVelocityIterations = 12;
+            Time.fixedDeltaTime = 0.01333f; //(75fps). default is .2 (60fps)
+            Time.maximumDeltaTime = .15f; // Default is .33
         }
     }
 }
